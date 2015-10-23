8a6624177019331b341089320f8d47479bde184d
KurlesHS <kahshs@gmail.com>
2015-10-23 14:51:30 +0300

    temp

--- app/src/main/java/com/horrosoft/viotimer/firmware/updater/app/MainActivity.java
+++ app/src/main/java/com/horrosoft/viotimer/firmware/updater/app/MainActivity.java
@@ -3,23 +3,36 @@ package com.horrosoft.viotimer.firmware.updater.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import com.horrosoft.viotimer.firmware.updater.app.bluetooth.DeviceListActivity;
+import com.lamerman.FileDialog;
+import com.lamerman.SelectionMode;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 
 public class MainActivity extends Activity {
 
     private static final int REQUEST_BT_DEVICE_MAC_ADDRESS = 0;
+    private static final int LOAD_FILE_ID = 1;
+
+    private Button makeAllHappyButton;
+    private Button loadFirmwareButton;
+    private byte[] firmware = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button makeAllHappyButton = (Button) findViewById(R.id.make_all_happy_button);
+        makeAllHappyButton = (Button) findViewById(R.id.make_all_happy_button);
+        loadFirmwareButton = (Button) findViewById(R.id.load_button);
         final Activity activity = this;
 
         makeAllHappyButton.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +40,23 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(activity, DeviceListActivity.class);
                 startActivityForResult(intent, REQUEST_BT_DEVICE_MAC_ADDRESS);
+            }
+        });
+
+        loadFirmwareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), FileDialog.class);
+                intent.putExtra(FileDialog.START_PATH, Environment.getExternalStorageDirectory().getPath());
+                intent.putExtra(FileDialog.SELECTION_MODE, SelectionMode.MODE_CREATE);
+
+                //can user select directories or not
+                intent.putExtra(FileDialog.CAN_SELECT_DIR, false);
+
+                //alternatively you can set file filter
+                intent.putExtra(FileDialog.FORMAT_FILTER, new String[]{"tmr"});
+                startActivityForResult(intent, LOAD_FILE_ID);
+
             }
         });
     }
@@ -55,14 +85,27 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        TextView tv = (TextView) findViewById(R.id.textView);
+        if (data == null) {
+            return;
+        }
         String value = "something goes wrong";
-        if (requestCode == REQUEST_BT_DEVICE_MAC_ADDRESS && resultCode == RESULT_OK) {
-            value = data.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        if (requestCode == REQUEST_BT_DEVICE_MAC_ADDRESS) {
+            if (resultCode == RESULT_OK) {
+                value = data.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+            }
+        } else if (requestCode == LOAD_FILE_ID && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FileDialog.RESULT_PATH);
+            File file = new File(filePath);
+            firmware = new byte[(int)file.length()];
+            try {
+                DataInputStream ds = new DataInputStream((new FileInputStream(file)));
+                ds.readFully(firmware);
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+                firmware = null;
+            }
         }
-        if (tv != null) {
-            tv.setText(value);
-        }
+
         super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/horrosoft/viotimer/firmware/updater/app/bluetooth/DeviceListActivity.java
+++ app/src/main/java/com/horrosoft/viotimer/firmware/updater/app/bluetooth/DeviceListActivity.java
@@ -198,7 +198,10 @@ public class DeviceListActivity extends Activity {
                 // Get the BluetoothDevice object from the Intent
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // If it's already paired, skip it, because it's been listed already
+
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                } else {
                     mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 }
             // When discovery is finished, change the Activity title
